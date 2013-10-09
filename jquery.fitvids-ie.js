8db4156cdb3e1ab008fc337318999ed0a24b4c41
stephencarr <stephencarr@gmail.com>
2013-10-08 20:22:34 -0700

    Update and rename jquery.fitvids.js to jquery.fitvids-ie.js

--- jquery.fitvids-ie.js
+++ jquery.fitvids-ie.js
@@ -1,13 +1,13 @@
 /*global jQuery */
 /*jshint multistr:true browser:true */
 /*!
-* FitVids 1.0.3
+* FitVids-ie 1.0.0
 *
+* Credit to original FitVids author davatron5000 - https://github.com/davatron5000/FitVids.js
 * Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
 * Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
 * Released under the WTFPL license - http://sam.zoy.org/wtfpl/
 *
-* Date: Thu Sept 01 18:00:00 2011 -0500
 */
 
 (function( $ ){
